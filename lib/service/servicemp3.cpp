aad4f07a18f49339d8db120e9e65b0a0767f2c19
christophecvr <stefansat@telenet.be>
2017-02-06 19:53:39 +0100

    Changed sync setting and position taking.
     The recent changes of dvbmediasink concerning EOS ,
     Does make it possible do change postion taking and
     also sync settings. For streamed media now position
     will always be taken from gstreamer pipeline self.
     Then the audio can be synchronized on the video by gstreamer.
     Most probably the position taking for file media will follow the same
     in future just some more tests to run on that.
    
    	modified:   lib/service/servicemp3.cpp
    
    Signed-off-by: Taapat <taapat@gmail.com>

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -21,7 +21,7 @@
 #include <gst/pbutils/missing-plugins.h>
 #include <sys/stat.h>
 
-#define HTTP_TIMEOUT 15
+#define HTTP_TIMEOUT 10
 
 /*
  * UNUSED variable from service reference is now used as buffer flag for gstreamer
@@ -610,17 +610,30 @@ eServiceMP3::eServiceMP3(eServiceReference ref):
 				g_object_set(dvb_audiosink, "e2-sync", TRUE, NULL);
 				g_object_set(dvb_audiosink, "e2-async", TRUE, NULL);
 			}
+			else if(m_sourceinfo.is_streaming)
+			{
+				g_object_set(dvb_audiosink, "e2-sync", TRUE, NULL);
+				g_object_set(dvb_audiosink, "e2-async", FALSE, NULL);
+			}
 			else
 			{
-				g_object_set(dvb_audiosink, "e2-sync", FALSE, NULL);
-				g_object_set(dvb_audiosink, "e2-async", FALSE, NULL);
+				g_object_set(dvb_audiosink, "e2-sync", TRUE, NULL);
+				g_object_set(dvb_audiosink, "e2-async", TRUE, NULL);
 			}
 			g_object_set(m_gst_playbin, "audio-sink", dvb_audiosink, NULL);
 		}
 		if(dvb_videosink && !m_sourceinfo.is_audio)
 		{
-			g_object_set(dvb_videosink, "e2-sync", FALSE, NULL);
-			g_object_set(dvb_videosink, "e2-async", FALSE, NULL);
+			if(m_sourceinfo.is_streaming)
+			{
+				g_object_set(dvb_videosink, "e2-sync", FALSE, NULL);
+				g_object_set(dvb_videosink, "e2-async", TRUE, NULL);
+			}
+			else
+			{
+				g_object_set(dvb_videosink, "e2-sync", FALSE, NULL);
+				g_object_set(dvb_videosink, "e2-async", FALSE, NULL);
+			}
 			g_object_set(m_gst_playbin, "video-sink", dvb_videosink, NULL);
 		}
 		/*
@@ -941,7 +954,7 @@ RESULT eServiceMP3::seekToImpl(pts_t to)
 		GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE))
 #else
 	m_last_seek_pos = to * 11111LL;
-	if (!gst_element_seek (m_gst_playbin, m_currentTrickRatio, GST_FORMAT_TIME, (GstSeekFlags)(GST_SEEK_FLAG_FLUSH | GST_SEEK_FLAG_KEY_UNIT),
+	if (!gst_element_seek (m_gst_playbin, m_currentTrickRatio, GST_FORMAT_TIME, (GstSeekFlags)(GST_SEEK_FLAG_FLUSH | GST_SEEK_FLAG_KEY_UNIT | GST_SEEK_FLAG_SNAP_BEFORE),
 		GST_SEEK_TYPE_SET, m_last_seek_pos,
 		GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE))
 #endif
@@ -1192,7 +1205,7 @@ RESULT eServiceMP3::getPlayPosition(pts_t &pts)
 	if ((pos = get_pts_pcrscr()) > 0)
 		pos *= 11111LL;
 #else
-	if ((dvb_audiosink || dvb_videosink) && !m_paused)
+	if ((dvb_audiosink || dvb_videosink) && !m_paused && !m_sourceinfo.is_streaming)
 	{
 		if (m_sourceinfo.is_audio)
 		{
@@ -1202,9 +1215,9 @@ RESULT eServiceMP3::getPlayPosition(pts_t &pts)
 		}
 		else
 		{
-			g_signal_emit_by_name(dvb_audiosink, "get-decoder-time", &pos);
+			g_signal_emit_by_name(dvb_videosink, "get-decoder-time", &pos);
 			if (!GST_CLOCK_TIME_IS_VALID(pos) || 0)
-				 g_signal_emit_by_name(dvb_videosink, "get-decoder-time", &pos);
+				 g_signal_emit_by_name(dvb_audiosink, "get-decoder-time", &pos);
 			if(!GST_CLOCK_TIME_IS_VALID(pos))
 				return -1;
 		}
